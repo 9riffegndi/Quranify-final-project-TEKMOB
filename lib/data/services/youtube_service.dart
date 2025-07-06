3e72b4a8e7927b2ca980a1d23461984c79a0fd12
9riffegndi <marrief19@gmail.com>
2025-07-06 11:27:29 +0700

    fix: bg vidio section

--- lib/data/services/youtube_service.dart
+++ lib/data/services/youtube_service.dart
@@ -1,17 +1,23 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../../config/youtube_config.dart';
 
 class YouTubeService {
-  // Using the provided API key
-  static const String _apiKey = 'AIzaSyD1NQw6MHD9HAPIzYE52UN_VqmMpmzh62A';
-  static const String _baseUrl = 'https://www.googleapis.com/youtube/v3';
+  // Using API key and base URL from config
+  final String _apiKey = YouTubeConfig.apiKey;
+  final String _baseUrl = YouTubeConfig.baseUrl;
 
   // Fetch Islamic study videos
   Future<List<Map<String, dynamic>>> getIslamicStudyVideos() async {
     try {
+      // Select a random keyword from the Islamic keywords list
+      final keyword =
+          YouTubeConfig.islamicKeywords[DateTime.now().microsecond %
+              YouTubeConfig.islamicKeywords.length];
+
       // Set up search parameters for Islamic study content in Indonesian
       final url = Uri.parse(
-        '$_baseUrl/search?part=snippet&maxResults=10&q=kajian islam&type=video&relevanceLanguage=id&key=$_apiKey',
+        '$_baseUrl/search?part=snippet&maxResults=10&q=$keyword&type=video&relevanceLanguage=id&key=$_apiKey',
       );
 
       final response = await http.get(url);
@@ -75,6 +81,96 @@ class YouTubeService {
       print('YouTube API Error: $e');
       return _getDummyVideos(); // Return dummy data in case of error
     }
+  }
+
+  // Fetch videos from a specific Islamic channel
+  Future<List<Map<String, dynamic>>> getChannelVideos(String channelKey) async {
+    try {
+      // Get the channel ID from the config
+      final channelId = YouTubeConfig.islamicChannels[channelKey];
+
+      if (channelId == null) {
+        print('Invalid channel key: $channelKey');
+        return _getDummyVideos();
+      }
+
+      // Set up search parameters for videos from this channel
+      final url = Uri.parse(
+        '$_baseUrl/search?part=snippet&maxResults=10&channelId=$channelId&order=date&type=video&key=$_apiKey',
+      );
+
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final items = data['items'] as List;
+
+        // Process results similar to getIslamicStudyVideos
+        // Get the video IDs to fetch duration info
+        final videoIds = items
+            .map((item) => item['id']['videoId'] as String)
+            .join(',');
+
+        // Get video details for duration
+        final videoDetailsUrl = Uri.parse(
+          '$_baseUrl/videos?part=contentDetails,statistics&id=$videoIds&key=$_apiKey',
+        );
+
+        final detailsResponse = await http.get(videoDetailsUrl);
+
+        if (detailsResponse.statusCode == 200) {
+          final detailsData = json.decode(detailsResponse.body);
+          final detailsItems = detailsData['items'] as List;
+
+          // Map details to the search results
+          final videos = <Map<String, dynamic>>[];
+
+          for (var i = 0; i < items.length; i++) {
+            final searchItem = items[i];
+            final videoId = searchItem['id']['videoId'];
+
+            // Find matching detail item
+            final detailItem = detailsItems.firstWhere(
+              (element) => element['id'] == videoId,
+              orElse: () => null,
+            );
+
+            if (detailItem != null) {
+              final snippet = searchItem['snippet'];
+              final details = detailItem['contentDetails'];
+              final statistics = detailItem['statistics'];
+
+              videos.add({
+                'id': videoId,
+                'title': snippet['title'],
+                'channelName': snippet['channelTitle'],
+                'thumbnailUrl': snippet['thumbnails']['high']['url'],
+                'duration': _formatDuration(details['duration']),
+                'views': _formatViews(statistics['viewCount'] ?? '0'),
+                'uploadedAt': _formatUploadDate(snippet['publishedAt']),
+              });
+            }
+          }
+
+          return videos;
+        }
+      }
+
+      return _getDummyVideos(); // Return dummy data in case of error
+    } catch (e) {
+      print('YouTube Channel API Error: $e');
+      return _getDummyVideos();
+    }
+  }
+
+  // Get list of available Islamic channels
+  Map<String, String> getAvailableIslamicChannels() {
+    return YouTubeConfig.islamicChannels;
+  }
+
+  // Get list of Islamic keywords
+  List<String> getIslamicKeywords() {
+    return YouTubeConfig.islamicKeywords;
   }
 
   // Format ISO 8601 duration to human-readable format
@@ -145,13 +241,14 @@ class YouTubeService {
 
   // Fallback method that returns dummy data when API fails
   List<Map<String, dynamic>> _getDummyVideos() {
+    print('Menggunakan data dummy karena API tidak tersedia atau error');
     return [
       {
         'id': 'example1',
         'title': 'Kajian Tafsir Al-Quran Surat Al-Baqarah',
         'channelName': 'Ustadz Abdul Somad',
         'duration': '45:22',
-        'thumbnailUrl': 'https://i.ytimg.com/vi/example1/maxresdefault.jpg',
+        'thumbnailUrl': 'assets/images/masque.png', // Using app assets
         'views': '245K',
         'uploadedAt': '2 minggu yang lalu',
       },
@@ -160,11 +257,37 @@ class YouTubeService {
         'title': 'Memahami Hadist Shahih Bukhari',
         'channelName': 'Ustadz Adi Hidayat',
         'duration': '38:15',
-        'thumbnailUrl': 'https://i.ytimg.com/vi/example2/maxresdefault.jpg',
+        'thumbnailUrl': 'assets/images/logo.png', // Using app assets
         'views': '128K',
         'uploadedAt': '3 minggu yang lalu',
       },
-      // Add more dummy videos here
+      {
+        'id': 'example3',
+        'title': 'Tafsir Surah Yasin',
+        'channelName': 'Ustadz Hanan Attaki',
+        'duration': '52:10',
+        'thumbnailUrl': 'assets/images/ngaji.png', // Using app assets
+        'views': '320K',
+        'uploadedAt': '1 bulan yang lalu',
+      },
+      {
+        'id': 'example4',
+        'title': 'Memahami Asmaul Husna',
+        'channelName': 'Ustadz Felix Siauw',
+        'duration': '41:25',
+        'thumbnailUrl': 'assets/images/profile.png', // Using app assets
+        'views': '180K',
+        'uploadedAt': '3 minggu yang lalu',
+      },
+      {
+        'id': 'example5',
+        'title': 'Kisah Para Nabi dan Rasul',
+        'channelName': 'Ustadz Khalid Basalamah',
+        'duration': '58:30',
+        'thumbnailUrl': 'assets/images/masque.png', // Using app assets
+        'views': '275K',
+        'uploadedAt': '1 bulan yang lalu',
+      },
     ];
   }
 }
